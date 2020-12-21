ec32371b4a7f73db1c86ba80716fe7792ec9e6f2
azizmousa <azizmousa1010@gmail.com>
2020-12-22 01:03:13 +0200

    implement the find method

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -7,7 +7,7 @@ class BinarySearchTree:
 	def __init__(self):
 		self.__root = None
 
-
+	# add method that kick off the adding methodology
 	def add(self, value):
 		node = Node(value)
 		if 	self.__root == None:
@@ -15,6 +15,9 @@ class BinarySearchTree:
 		else:
 			self.__addNode(node, self.__root)
 
+
+	# the recursive add method
+	# performe the add algorithm
 	def __addNode(self, inp_node, current):
 		if inp_node.value < current.value:
 			if current.left_child == None:
@@ -30,17 +33,45 @@ class BinarySearchTree:
 				self.__addNode(inp_node, current.right_child)
 
 
+	# method to kick off the tree visiting methodology (in order travers)
 	def in_order_travers(self):
 		self.__visit_inorder(self.__root)
 
 
+	# method to perform  the process functionality to the travers operation
 	def __process(self, node):
 		print(node.value)
 
 
+	# the recursive visiting method
+	# perform in order travers
 	def __visit_inorder(self, current):
 		if current == None:
 			return
 		self.__visit_inorder(current.left_child)
 		self.__process(current)
 		self.__visit_inorder(current.right_child)
+
+
+	def find(self, value):
+		node = self.__find_node(value)
+		if node == None:
+			return False
+		return True 
+
+
+	# method to find the node of the specified value
+	def __find_node(self, value):
+		current = self.__root
+		if current == value:
+			return current
+		else:
+			while current != None:
+				if value < current.value:
+					current = current.left_child
+				elif value > current.value:
+					current = current.right_child
+				else:
+					break
+
+			return current
