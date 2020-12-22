7428317c52caadab416130f0a6f2bf6ce97ba5bc
azizmousa <azizmousa1010@gmail.com>
2020-12-22 02:40:03 +0200

    implement the remove method

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -75,3 +75,59 @@ class BinarySearchTree:
 					break
 
 			return current
+
+	# method to remove value from the tree without change the tree structure
+	def remove(self, value):
+		node = self.__find_node(value)
+		parent = node.parent
+		# case 1: the node not exist
+		if node == None:
+			return False
+		# case 2: the node is leaf node
+		elif node.left_child == None and node.right_child == None:
+			if parent.left_child == node:
+				parent.left_child = None
+			else:
+				parent.right_child = None
+		# case 3:   the node not leaf node
+		# case 3.1: the node has no right child
+		elif node.right_child == None:
+			if parent.left_child == node:
+				parent.left_child = node.left_child
+			else:
+				parent.right_child = node.left_child
+		# case 3.2: the node has right child, 
+		# 			and that right child has no left child
+		elif node.right_child != None and node.right_child.left_child == None:
+			node.right_child.left_child = node.left_child
+			if parent.left_child == node:
+				parent.left_child = node.right_child
+			else:
+				parent.right_child = node.right_child
+		# case 3.3: the node has right child,
+		# 			and that right child has left child
+		elif node.right_child != None and node.right_child.left_child != None:
+			current = node
+			# find the most left child
+			while current.left_child != None:
+				current = current.left_child
+			# change the parent to the current parent
+			parent = current.parent
+			if parent.left_child == node:
+				parent.left_child = None
+			else:
+				parent.right_child = None
+			# just repleace the node value with the current value
+			node.value = current.value
+
+		return True
+
+
+
+
+
+
+
+
+
+
